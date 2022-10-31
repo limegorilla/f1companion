28db9492c17b7533e93a11552f0300e1566bd53a
Liam Doyle <liamdoyle@limegorilla.co.uk>
2022-10-31 09:06:19 +0000

    Initial Commit

---
+++ f1companion/f1companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  f1companionApp.swift
+//  f1companion
+//
+//  Created by Liam Doyle on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct f1companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
