2c3fca5ee2c701334443194dff4b96234f3d0904
Liam Doyle <liamdoyle@limegorilla.co.uk>
2022-11-01 17:40:49 +0000

    Add AppIcon images and initial list POC

--- f1companion/ContentView.swift
+++ f1companion/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    let Meetings: [Meeting]
+}
+
+struct Meeting: Codable {
+    let Key: Int
+    let Code: String
+    let OfficialName: String
+    let Location: String
+    let Sessions: [Session]
+}
+
+struct Session: Codable {
+    let Key: Int
+    let `Type`: String
+}
+
+
 struct ContentView: View {
+
+    @State private var results = [Meeting]()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(results, id: \.Key) { item in
+            VStack(alignment: .leading) {
+                Text(item.OfficialName)
+                    .font(.headline)
+                Text(item.Location)
+                ForEach(item.Sessions, id: \.Key) { meeting in
+                    Text(meeting.`Type`)
+                }
+            }
         }
-        .padding()
+        .navigationTitle("Test")
+        .task {
+            await loadData()
+            // Waits for loadData (network req) to complete before loading UI
+        }
+        
+    }
+    func loadData() async {
+    
+        guard let url = URL(string: "https://livetiming.formula1.com/static/2022/Index.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            print(data)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.Meetings
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
